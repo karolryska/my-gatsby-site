89e53fec09e16b1fb19ba0560fd3038608a03d1d
Karol Ryska <karolryska@gmail.com>
2021-09-12 18:31:47 +0200

    chore: add layout component and refactor pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { Link } from 'gatsby';
+import Layout from "../components/layout";
 
 const IndexPage = () => {
-  return (
-    <main>
-      <title>Home Page</title>
-      <Link to="/about">About</Link>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
-  )
+	return (
+    	<Layout pageTitle="Home Page">
+      		<p>I'm making this by following the Gatsby Tutorial.</p>
+    	</Layout>
+  	)
 };
 
-export default IndexPage
+export default IndexPage;
