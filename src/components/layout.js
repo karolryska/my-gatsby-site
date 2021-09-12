d2a4d6edaee66e3316aa2ed169208d5a36210cd3
Karol Ryska <karolryska@gmail.com>
2021-09-12 19:33:39 +0200

    style: add css modules to layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import * as styles from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
-  return (
-    <div>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-      <main>
-        <h1>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+        <title>{pageTitle}</title>
+        <nav>
+            <ul className={styles.navLinks}>
+                <li className={styles.navLinkItem}>
+                    <Link to="/" className={styles.navLinkText}>Home</Link>
+                </li>
+                <li className={styles.navLinkItem}>
+                    <Link to="/about" className={styles.navLinkText}>About</Link>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <h1 className={styles.heading}>{pageTitle}</h1>
+            {children}
+        </main>
+        </div>
+    )
 };
 
 export default Layout;
