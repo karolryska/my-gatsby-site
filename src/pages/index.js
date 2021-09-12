a4049d56e841ebad961e60561e02e15071df6ec9
Karol Ryska <karolryska@gmail.com>
2021-09-12 15:37:37 +0200

    feat: add basic routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from 'gatsby';
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
+      <Link to="/about">About</Link>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
