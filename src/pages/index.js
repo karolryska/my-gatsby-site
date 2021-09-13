07ac475b5c69f1b7235fdd94e3a1e01ab3180b65
Karol Ryska <karolryska@gmail.com>
2021-09-13 21:31:11 +0200

    feat: add mdx transformer plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
     	<Layout pageTitle="Home Page">
       		<p>I'm making this by following the Gatsby Tutorial.</p>
 			  <StaticImage 
-			  	alt="corgi" 
+			  	alt="silver car" 
 			  	src="../images/car.jpg" 
 			  />
     	</Layout>
